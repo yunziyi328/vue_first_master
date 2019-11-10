1c9bcb29b2d279a2febe1cee66b276413f09871e
yunziyi328 <57064047+yunziyi328@users.noreply.github.com>
2019-11-11 00:16:15 +0900

    computed计算属性

--- app.js
+++ app.js
@@ -3,21 +3,31 @@ new Vue({
     el: "#vue-app", // element 对应index.html的id="vue-app" 可操作的id容器
     data() {
         return {
-            name: '',
-            age: 32
+            a: 0,
+            b: 0,
+            age: 30
         };
     },
-    methods: {
-        onNameEnter() {
-            this.name = this.$refs.refName.value;
-        },
-        onAgeEnter() {
-            this.age = this.$refs.refAge.value;
-        }
+    methods: { // methods中的所有方法都会被运行
+        // AddToA() {
+        //     console.log('AddToA');
+        //     return this.age + this.a;
+
+        // },
+        // AddToB() {
+        //     console.log('AddToB');
+        //     return this.age + this.b;
+        // }
     },
-    watch: {
-        age(val, oldVal) {
-            console.log(val, oldVal);
+    computed: { // 计算属性必须要有返回值 return
+        AddToA() {
+            console.log('AddToA');
+            return this.age + this.a;
+
+        },
+        AddToB() {
+            console.log('AddToB');
+            return this.age + this.b;
         }
     }
 });
