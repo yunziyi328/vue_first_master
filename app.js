3bfc98b1e0771d62388de80a7f43fc7deb85b53f
yunziyi328 <57064047+yunziyi328@users.noreply.github.com>
2019-11-11 22:39:13 +0900

    css动态样式绑定

--- app.js
+++ app.js
@@ -3,31 +3,17 @@ new Vue({
     el: "#vue-app", // element 对应index.html的id="vue-app" 可操作的id容器
     data() {
         return {
-            a: 0,
-            b: 0,
-            age: 30
+            mrChangeColor: true,
+            mrchangeLength: false
         };
     },
-    methods: { // methods中的所有方法都会被运行
-        // AddToA() {
-        //     console.log('AddToA');
-        //     return this.age + this.a;
-
-        // },
-        // AddToB() {
-        //     console.log('AddToB');
-        //     return this.age + this.b;
-        // }
-    },
-    computed: { // 计算属性必须要有返回值 return
-        AddToA() {
-            console.log('AddToA');
-            return this.age + this.a;
-
-        },
-        AddToB() {
-            console.log('AddToB');
-            return this.age + this.b;
+    methods: {},
+    computed: {
+        comColor() {
+            return {
+                changeColor: this.mrChangeColor,
+                changeLength: this.mrchangeLength
+            };
         }
     }
 });
