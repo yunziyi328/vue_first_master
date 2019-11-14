f524cf1b49ddd25391bc39e235afadeb7765ae4b
yunziyi328 <57064047+yunziyi328@users.noreply.github.com>
2019-11-14 22:51:15 +0900

    component的全局使用方法

--- app.js
+++ app.js
@@ -1,5 +1,35 @@
-// // 实例化vue对象
-const vueOne = new Vue({
+// 全局变量
+let dataAll = {
+    name: 'XXX',
+    wechat: '27732357'
+};
+
+// 创建全局组件
+Vue.component('Greeting', {
+    // html模板
+    template: `
+    <p>
+    这是全局组件，可以在任何实例的容器中使用
+    私の名前： {{name}}, wechat: {{wechat}}
+    <button @click="changeName">改名</button>
+    </p>
+    `,
+    // 属性 方法等
+    data() {
+        // return {
+        //     name: 'XXX',
+        //     wechat: '27732357'
+        // };
+        return dataAll;
+    },
+    methods: {
+        changeName() {
+            this.name = "YYY"
+        }
+    }
+})
+
+const one = new Vue({
     el: "#vue-app-one", // element 对应index.html的id="vue-app" 可操作的id容器
     data() {
         return {
@@ -10,7 +40,7 @@ const vueOne = new Vue({
     computed: {}
 });
 
-const vueTwo = new Vue({
+const two = new Vue({
     el: "#vue-app-two", // element 对应index.html的id="vue-app" 可操作的id容器
     data() {
         return {
