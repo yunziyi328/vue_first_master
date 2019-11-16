5a18cc4e43526b2a5e4f1bd9008cfe79bf9a88fa
yunziyi328 <57064047+yunziyi328@users.noreply.github.com>
2019-11-16 17:47:59 +0900

    fetch請求

--- app.js
+++ app.js
@@ -1,59 +1,36 @@
-// 全局变量
-let dataAll = {
-    name: 'XXX',
-    wechat: '27732357'
-};
-
-// 创建全局组件
-Vue.component('Greeting', {
-    // html模板
-    template: `
-    <p>
-    这是全局组件，可以在任何实例的容器中使用
-    私の名前： {{name}}, wechat: {{wechat}}
-    <button @click="changeName">改名</button>
-    </p>
-    `,
-    // 属性 方法等
+new Vue({
+    el: "#vue-app", // element 对应index.html的id="vue-app" 可操作的id容器
     data() {
-        // return {
-        //     name: 'XXX',
-        //     wechat: '27732357'
-        // };
-        return dataAll;
+        return {
+            todos: [],
+            todo: {
+                title: '',
+                completed: false
+            }
+        };
+    },
+    // 勾的函数 在页面还没有渲染之前就可以执行的东西
+    mounted() {
+        // fetch api请求接口
+        fetch('http://jsonplaceholder.typicode.com/todos').then(res => {
+            return res.json()
+        }).then(todos => {
+            this.todos = todos;
+        });
     },
     methods: {
-        changeName() {
-            this.name = "YYY"
+        onSubmit() {
+            fetch('http://jsonplaceholder.typicode.com/todos', {
+                method: 'POST',
+                body: JSON.stringify(this.todo),
+                headers: {
+                    'Content-type': 'application / json'
+                }
+            }).then(res => {
+                return res.json()
+            }).then(todo => {
+                this.todos.unshift(todo);
+            });
         }
     }
-})
-
-const one = new Vue({
-    el: "#vue-app-one", // element 对应index.html的id="vue-app" 可操作的id容器
-    data() {
-        return {
-            title: '第一个容器'
-        };
-    },
-    methods: {},
-    computed: {}
 });
-
-const two = new Vue({
-    el: "#vue-app-two", // element 对应index.html的id="vue-app" 可操作的id容器
-    data() {
-        return {
-            title: '第二个容器'
-        };
-    },
-    methods: {
-        ChangeAppOne() {
-            vueOne.title = "这是vueOne的title"
-        }
-    },
-    computed: {}
-});
-
-// 在外部改变vueTwo的内容
-// vueTwo.title = "这是vueTwo的title"
