31e43590fe95c0a60a5fe13a3e1ea1a865258473
yunziyi328 <57064047+yunziyi328@users.noreply.github.com>
2019-11-17 16:46:42 +0900

    lesson-17_axios請求

--- app.js
+++ app.js
@@ -12,25 +12,35 @@ new Vue({
     // 勾的函数 在页面还没有渲染之前就可以执行的东西
     mounted() {
         // fetch api请求接口
-        fetch('http://jsonplaceholder.typicode.com/todos').then(res => {
-            return res.json()
-        }).then(todos => {
-            this.todos = todos;
-        });
+        // fetch('http://jsonplaceholder.typicode.com/todos').then(res => {
+        //     return res.json()
+        // }).then(todos => {
+        //     this.todos = todos;
+        // });
+
+        // axios get请求接口
+        axios.get('http://jsonplaceholder.typicode.com/todos').then
+            (res => {
+                this.todos = res.data;
+            });
     },
     methods: {
         onSubmit() {
-            fetch('http://jsonplaceholder.typicode.com/todos', {
-                method: 'POST',
-                body: JSON.stringify(this.todo),
-                headers: {
-                    'Content-type': 'application / json'
-                }
-            }).then(res => {
-                return res.json()
-            }).then(todo => {
-                this.todos.unshift(todo);
-            });
+            axios.post('http://jsonplaceholder.typicode.com/todos', this.todo).then(res => {
+                // console.log(res.data);
+                this.todos.unshift(res.data);
+            })
+            // fetch('http://jsonplaceholder.typicode.com/todos', {
+            //     method: 'POST',
+            //     body: JSON.stringify(this.todo),
+            //     headers: {
+            //         'Content-type': 'application/json'
+            //     }
+            // }).then(res => {
+            //     return res.json();
+            // }).then(todo => {
+            //     this.todos.unshift(todo);
+            // });
         }
     }
 });
